2882b1f44b06b322dd1a95563e14234c26d2ef7f
Elliot Morales <elliot@admincentralmail.com>
2018-03-08 11:31:27 +0100

    Better aproach removing the sleep 10 and waiting for confd to start rabbitmq

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+COUNT=0;
 set -e
-sleep 10
+while [ ! -f ${DIR}/.configured ]; then
+        echo "Not configured already"
+	if [ ${COUNT} -gt 10 ]; then
+		echo "After 10 seconds, confd has not configured rabbitmq, continue as default"
+		break;
+	fi
+	sleep 1
+	COUNT=$(( $COUNT + 1 ))
+done
 exec /usr/local/bin/docker-entrypoint.sh rabbitmq-server
